e9488e9876d38977206f026c4bce19148d44a019
mxmlnkn <mxmlnkn@github.de>
2016-02-09 05:45:22 +0100

    spark script

---
+++ MontePi/multiNode/start-cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+MASTER=$1
+HOST=$(hostname)
+
+if [ "$HOST" == "$MASTER" ]; then
+    $SPARK_HOME/sbin/start-master.sh 
+fi
+$SPARK_HOME/sbin/start-slave.sh spark://$MASTER:7077  
+
+tail -f /dev/null #wait forever
+
