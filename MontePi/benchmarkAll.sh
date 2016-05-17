50d1dade09a4b2e7476c8a8baceaf4c9a0761897
mxmlnkn <mxmlnkn@github.de>
2016-05-17 04:29:21 +0200

    run implementation benchmark also on taurus

--- MontePi/benchmarkAll.sh
+++ MontePi/benchmarkAll.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-
+# E.g. use this in
+# salloc -p gpu2-interactive --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --gres=gpu:1 --time=1:00:00
+taurus=$1
+if [ -z "$taurus" ]; then
+    taurus=0
+    gpusPerNode=1
+else
+    taurus=1
+    gpusPerNode=4
+fi
+RUN="time -p"
+if [ "$taurus" -eq 1 ]; then RUN="srun $RUN"; fi
 
 i0=256
 imax=268435456000
@@ -18,22 +29,25 @@ trap "rm tmp.log" EXIT
 
 # Make all if changed
 #make -C singleNode/singleCore/java/
-#make -C singleNode/singleCore/scala/
+#make -C singleNode/singleCore/scala/s
 #make -C singleNode/singleGpu/cpp/
 #make -C singleNode/multiGpu/java/
 #make -C singleNode/multiGpu/scala/
 
 getTime() {
-    minutes=( $(sed -nr 's/real[[:space:]]*([0-9.]+)m([0-9.]+)s.*/\1/p' $1) )
-    seconds=( $(sed -nr 's/real[[:space:]]*([0-9.]+)m([0-9.]+)s.*/\2/p' $1) )
-    for (( i=0; i<${#seconds[@]}; i++ )); do
-        echo "${minutes[i]}*60+${seconds[i]}" | bc
-    done
+    # POSIX format has no 's' suffix but it is in seconds. In contrast
+    # to the time built-in of bash there also is no 1m2s format
+    # 'time' -p sleep 122s
+    #   real 122.00
+    #   user 0.00
+    #   sys 0.00
+    sed -nr 's/real[[:space:]]*([0-9.]+).*/\1/p' $1
 }
 
 logFolder="./raw-logs/benchmarks-$(date +%Y-%m-%d_%H-%M-%S)"
 mkdir -p "$logFolder"
 resultsFile="$logFolder/results.log"
+echo "You can find the output in '$resultsFile'"
 printf "nDiceRolls  |  1 Core (java)  |  1 Core (scala)  | 1 GPU (C++)  | 1 GPU (Java)  | 1 GPU (Scala)\n" | tee $resultsFile
 
 nRollsList=( $(python <<EOF
@@ -59,148 +73,59 @@ for (( i=i0; i<imax; i=3*i/2  )); do
     nDiceRolls=$((i-i%256))
     allDiceRolls+=( $nDiceRolls )
 
-    #echo "nDiceRolls = $nDiceRolls"
-    # single core java
-    if [ ${#timesSingleCoreJava[@]} -eq 0 ] ||
-       ( [ $( echo "${timesSingleCoreJava[-1]} < $tmaxCpu" | bc ) -eq 1 ] && # 1 means true!
-         [ ! ${timesSingleCoreJava[-1]} == -1 ] )
-    then
-        (time java -jar singleNode/singleCore/java/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-        cat tmp.log >> "$logFolder/singleNode-singleCore-java.log"
-        timesSingleCoreJava+=( $(getTime tmp.log) )
-    else
-        timesSingleCoreJava+=( -1 )
-    fi
+    # because a[-1] does not work in older bash versions and also to
+    # cope with indirections
+    # after 1st eval: echo ${s[${#s[@]}-1]}
+    # Note that inside this script one eval could be saved by using "
+    # instead of ', but that wouldn't work when copy-pasting into a shell!
+    function last() {
+        if [ $(eval eval 'echo \${#$1[@]}') -gt 0 ]; then
+            eval eval 'echo \${$1[\${#$1[@]}-1]}'
+        fi
+    }
+    function append() {
+        x=$1
+        shift
+        while [ ! -z "$1" ]; do
+            eval eval '$x[ \${#$x[@]} ]=\$1'
+            shift
+        done
+    }
 
-    # single core scala
-    if [ ${#timesSingleCoreScala[@]} -eq 0 ] ||
-       ( [ $( echo "${timesSingleCoreScala[-1]} < $tmaxCpu" | bc ) -eq 1 ] && # 1 means true!
-         [ ! ${timesSingleCoreScala[-1]} == -1 ] )
-    then
-        (time java -jar singleNode/singleCore/scala/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-        cat tmp.log >> "$logFolder/singleNode-singleCore-scala.log"
-        timesSingleCoreScala+=( $(getTime tmp.log) )
-    else
-        timesSingleCoreScala+=( -1 )
-    fi
-
-    # single Gpu C++
-    if [ ${#timesSingleGpuCpp[@]} -eq 0 ] ||
-       ( [ $( echo "${timesSingleGpuCpp[-1]} < $tmaxGpu" | bc ) -eq 1 ] && # 1 means true!
-         [ ! ${timesSingleGpuCpp[-1]} == -1 ] )
-    then
-        (time singleNode/singleGpu/cpp/TestMonteCarloPiV2.exe $nDiceRolls 0) > tmp.log 2>&1
-        cat tmp.log >> "$logFolder/singleNode-singleGpu-Cpp.log"
-        timesSingleGpuCpp+=( $(getTime tmp.log) )
-    else
-        timesSingleGpuCpp+=( -1 )
-    fi
-
-    # single Gpu java
-    if [ ${#timesSingleGpuJava[@]} -eq 0 ] ||
-       ( [ $( echo "${timesSingleGpuJava[-1]} < $tmaxGpu" | bc ) -eq 1 ] && # 1 means true!
-         [ ! ${timesSingleGpuJava[-1]} == -1 ] )
-    then
-        (time java -jar singleNode/multiGpu/java/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-        cat tmp.log >> "$logFolder/singleNode-singleGpu-java.log"
-        timesSingleGpuJava+=( $(getTime tmp.log) )
-    else
-        timesSingleGpuJava+=( -1 )
-    fi
-
-    # single Gpu scala
-    if [ ${#timesSingleGpuScala[@]} -eq 0 ] ||
-       ( [ $( echo "${timesSingleGpuScala[-1]} < $tmaxGpu" | bc ) -eq 1 ] && # 1 means true!
-         [ ! ${timesSingleGpuScala[-1]} == -1 ] )
-    then
-        (time java -jar singleNode/multiGpu/scala/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-        cat tmp.log >> "$logFolder/singleNode-singleGpu-scala.log"
-        timesSingleGpuScala+=( $(getTime tmp.log) )
-    else
-        timesSingleGpuScala+=( -1 )
-    fi
-
-    printf "%15i%10.5f%10.5f%10.5f%10.5f%10.5f\n" ${allDiceRolls[-1]} ${timesSingleCoreJava[-1]} ${timesSingleCoreScala[-1]} ${timesSingleGpuCpp[-1]} ${timesSingleGpuJava[-1]} ${timesSingleGpuScala[-1]} | tee -a $resultsFile
-done
-
-sed -nr 's/Rolling the dice ([0-9]+) times resulted in pi ~ ([0-9.]+) and took (.*) seconds.*/\1   \2/p' "$logFolder/singleNode-singleGpu-java.log" > "$logFolder/error-scaling.log"
-
-#echo "" > results.log
-#for (( i=0; i<${#timesSingleGpuCpp[*]}; i=i+1 )); do
-#    printf "%10i%10.5f%10.5f%10.5f%10.5f%10.5f\n" ${allDiceRolls[i]} ${timesSingleCoreJava[i]} ${timesSingleCoreScala[i]} ${timesSingleGpuCpp[i]} ${timesSingleGpuJava[i]} ${timesSingleGpuScala[i]} >> results.log
-#done
-#cat results.log
-#
-#if [ -f benchmarks.log ]; then
-#    mv benchmarks.log benchmarks-$(date +%Y-%m-%d_%H-%M-%S).log
-#fi
-#if [ -f results.log ]; then
-#    mv results.log results-$(date +%Y-%m-%d_%H-%M-%S).log
-#fi
-
-exit
-
-# benchmark scaling with different work loads
-for (( i=i0; i<imax; i=3*i/2  )); do
-    nDiceRolls=$((i-i%256))
-    allDiceRolls+=( $nDiceRolls )
+    function measure() {
+        # $1: array name where to append result, also covers as log-name
+        # $2: command to run, e.g. 'java -jar ...'
+        # $3: log path
+        lastElem=$(last $1)
+        if [ -z $lastElem ] ||
+           ( [ $( echo "$lastElem < $tmaxCpu" | bc ) -eq 1 ] && # 1 means true!
+             [ ! $lastElem == -1 ] )
+        then
+            # without eval instead of the built-in time command /usr/bin/time
+            # would be used! -> works bash 4.3, but not in 4.1 -.-
+            # Therefore just use 'time' but with -p (posix) which has the
+            # same output as bash
+            ( $RUN $2 $nDiceRolls $3 ) > tmp.log 2>&1
+            cat tmp.log >> "$logFolder/$1.log"
+            append $1 $(getTime tmp.log)
+        else
+            append $1 -1
+        fi
+    }
 
     #echo "nDiceRolls = $nDiceRolls"
-    # single core java
-    (time java -jar singleNode/singleCore/java/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-    cat tmp.log >> "$logFolder/singleNode-singleCore-java.log"
-    #timesSingleCoreJava+=( $(sed -nr 's/Rolling the dice.*and took (.*) seconds.*/\1/p' benchmarks.log) )
-    timesSingleCoreJava+=( $(getTime tmp.log) )
+    measure timesSingleCoreJava  'java -jar singleNode/singleCore/java/MontePi.jar'
+    measure timesSingleCoreScala 'java -jar singleNode/singleCore/scala/MontePi.jar'
+    measure timesSingleGpuCpp    'singleNode/singleGpu/cpp/TestMonteCarloPiV2.exe' 0
+    measure timesSingleGpuJava   'java -jar singleNode/multiGpu/java/MontePi.jar'  0
+    measure timesSingleGpuScala  'java -jar singleNode/multiGpu/scala/MontePi.jar' 0
 
-    # single core scala
-    (time java -jar singleNode/singleCore/scala/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-    cat tmp.log >> "$logFolder/singleNode-singleCore-scala.log"
-    timesSingleCoreScala+=( $(getTime tmp.log) )
-
-    # single Gpu C++
-    #(time singleNode/singleGpu/cpp/TestMonteCarloPiV2.exe $nDiceRolls 0) > tmp.log 2>&1
-    cat tmp.log >> "$logFolder/singleNode-singleGpu-Cpp.log"
-    timesSingleGpuCpp+=( $(getTime tmp.log) )
-
-    # single Gpu java
-    #(time java -jar singleNode/multiGpu/java/MontePi.jar $nDiceRolls 0) > tmp.log 2>&1
-    cat tmp.log >> "$logFolder/singleNode-singleGpu-java.log"
-    timesSingleGpuJava+=( $(getTime tmp.log) )
-
-    # single Gpu scala
-    #(time scala singleNode/multiGpu/scala/MontePiGPU.jar $nDiceRolls 0) > tmp.log 2>&1
-    cat tmp.log >> "$logFolder/singleNode-singleGpu-scala.log"
-    timesSingleGpuScala+=( $(getTime tmp.log) )
-
-    printf "%15i%10.5f%10.5f%10.5f%10.5f%10.5f\n" ${allDiceRolls[-1]} ${timesSingleCoreJava[-1]} ${timesSingleCoreScala[-1]} ${timesSingleGpuCpp[-1]} ${timesSingleGpuJava[-1]} ${timesSingleGpuScala[-1]} | tee -a $resultsFile
+    # use last instead of [-1] because latter doesn't work on taurus -.-
+    printf "%15i%10.5f%10.5f%10.5f%10.5f%10.5f\n" \
+        $(last allDiceRolls)         \
+        $(last timesSingleCoreJava)  \
+        $(last timesSingleCoreScala) \
+        $(last timesSingleGpuCpp)    \
+        $(last timesSingleGpuJava)   \
+        $(last timesSingleGpuScala) | tee -a $resultsFile
 done
-    cat > tmp.slurm <<"EOF"
-#!/bin/bash
-nGpusPerNode=4
-iGpuToUse=$((SLURM_PROCID % nGpusPerNode ))
-echo $(hostname) Process ID: $SLURM_PROCID
-echo GPU device to use: $iGpuToUse
-./TestMonteCarloPiV2.exe 2684354560 $iGpuToUse
-EOF
-    chmod u+x tmp.slurm
-    srun tmp.slurm
-
-        taurusi2094 Process ID = 6
-        taurusi2094 Process ID = 5
-        taurusi2094 Process ID = 4
-        taurusi2094 Process ID = 7
-        taurusi2093 Process ID = 0
-        taurusi2093 Process ID = 1
-        taurusi2093 Process ID = 2
-        taurusi2093 Process ID = 3
-
-    cat > tmp.slurm <<"EOF"
-#!/bin/bash
-nGpusPerNode=4
-iGpuToUse=$((SLURM_PROCID % nGpusPerNode ))
-echo $(hostname) Process ID: $SLURM_PROCID
-echo GPU device to use: $iGpuToUse
-java -jar MontePi.jar 2684354560 $iGpuToUse
-EOF
-
-    srun tmp.slurm
