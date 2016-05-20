c2911dc32f6bf6cfcbbc9b15c8aaa48d600a39ae
mxmlnkn <mxmlnkn@github.de>
2016-05-21 00:41:52 +0200

    further test about partitioning

--- MontePi/partitionBy.scala
+++ MontePi/partitionBy.scala
@@ -3,16 +3,26 @@ spark-shell --master=$MASTER_ADDRESS
 
 import java.net.InetAddress
 import org.apache.spark.Partitioner
-val pairs = sc.parallelize( 0 to 4*12-1, 4*12-1 ).map( x=>(x,x) )
-
-
+class GroupPartitioner[V]( partitions: Int, elements: Int) extends Partitioner {
+    def numPartitions() : Int = partitions
+    def getPartition(key: Any): Int = {
+        val k = key.asInstanceOf[Int]
+        val nCoresPerNode = 12
+        val nNodes = ( partitions + nCoresPerNode - 1 ) / nCoresPerNode // ceilDiv
+        val part = ( k * nNodes  + k / nCoresPerNode ) % ( nNodes * nCoresPerNode )
+        part % partitions
+    }
+}
 class ExactPartitioner[V]( partitions: Int, elements: Int) extends Partitioner {
     def numPartitions() : Int = partitions
-    def getPartition(key: Any): Int = key.asInstanceOf[Int]
+    def getPartition(key: Any): Int = key.asInstanceOf[Int] % partitions
 }
 
 
-pairs.mapPartitionsWithIndex{(ip,it)=> it.map( (x) => { Thread.sleep(10); x+" : "+InetAddress.getLocalHost().getHostName()+" located in "+ip } )}.collect().foreach( println )
+sc.parallelize( 0 to 4*12-1, 4*12-1 ).map( x=>(x,x) ).
+    mapPartitionsWithIndex{(ip,it)=> it.map( (x) => {
+        Thread.sleep(10); x+" : "+InetAddress.getLocalHost().getHostName()+" located in "+ip } )}.
+    collect().foreach( println )
 
 (0,0)   : taurusi3094 located in 0
 (1,1)   : taurusi3092 located in 1
@@ -115,17 +125,7 @@ pairs.partitionBy( new ExactPartitioner(4*12,4*12) ).mapPartitionsWithIndex{(ip,
 (46,46) : taurusi3094 located in 46
 (47,47) : taurusi3092 located in 47
 
-
-class ExactPartitioner[V]( partitions: Int, elements: Int) extends Partitioner {
-    def numPartitions() : Int = partitions
-    def getPartition(key: Any): Int = {
-        val k = key.asInstanceOf[Int]
-        val nCoresPerNode = 12
-        val nNodes = ( partitions + nCoresPerNode - 1 ) / nCoresPerNode // ceilDiv
-        ( k * nNodes  + k / nCoresPerNode ) % ( nNodes * nCoresPerNode )
-    }
-}
-pairs.partitionBy( new ExactPartitioner(4*12,4*12) ).mapPartitionsWithIndex{(ip,it)=> it.map( (x) => { Thread.sleep(10); x+" : "+InetAddress.getLocalHost().getHostName()+" located in "+ip } )}.collect().foreach( println )
+pairs.partitionBy( new GroupPartitioner(4*12,4*12) ).mapPartitionsWithIndex{(ip,it)=> it.map( (x) => { Thread.sleep(10); x+" : "+InetAddress.getLocalHost().getHostName()+" located in "+ip } )}.collect().foreach( println )
 
 (0,0) : taurusi3099 located in 0
 (12,12) : taurusi3092 located in 1
@@ -233,3 +233,84 @@ pairs.partitionBy( new ExactPartitioner(4*12,4*12) ).mapPartitionsWithIndex{(ip,
 (46,46) : taurusi3094 located in 43
 (47,47) : taurusi3094 located in 47
 
+cat <<EOF | sed -nE 's|\(([0-9]*),[0-9]*\) : ([A-Za-z0-9]*) .*|\1 \2|p' | sort -n
+(0,0) : taurusi3147 located in 0
+(12,12) : taurusi3149 located in 1
+(24,24) : taurusi3148 located in 2
+(36,36) : taurusi3146 located in 3
+(1,1) : taurusi3147 located in 4
+(13,13) : taurusi3149 located in 5
+(25,25) : taurusi3148 located in 6
+(37,37) : taurusi3146 located in 7
+(2,2) : taurusi3147 located in 8
+(14,14) : taurusi3149 located in 9
+(26,26) : taurusi3148 located in 10
+(38,38) : taurusi3146 located in 11
+(3,3) : taurusi3147 located in 12
+(15,15) : taurusi3149 located in 13
+(27,27) : taurusi3148 located in 14
+(39,39) : taurusi3146 located in 15
+(4,4) : taurusi3147 located in 16
+(16,16) : taurusi3149 located in 17
+(28,28) : taurusi3148 located in 18
+(40,40) : taurusi3146 located in 19
+(5,5) : taurusi3147 located in 20
+(17,17) : taurusi3149 located in 21
+(29,29) : taurusi3148 located in 22
+(41,41) : taurusi3146 located in 23
+(6,6) : taurusi3147 located in 24
+(18,18) : taurusi3149 located in 25
+(30,30) : taurusi3148 located in 26
+(42,42) : taurusi3146 located in 27
+(7,7) : taurusi3147 located in 28
+(19,19) : taurusi3149 located in 29
+(31,31) : taurusi3148 located in 30
+(43,43) : taurusi3146 located in 31
+(8,8) : taurusi3147 located in 32
+(20,20) : taurusi3149 located in 33
+(32,32) : taurusi3148 located in 34
+(44,44) : taurusi3146 located in 35
+(9,9) : taurusi3147 located in 36
+(21,21) : taurusi3149 located in 37
+(33,33) : taurusi3148 located in 38
+(45,45) : taurusi3146 located in 39
+(10,10) : taurusi3147 located in 40
+(22,22) : taurusi3149 located in 41
+(34,34) : taurusi3148 located in 42
+(46,46) : taurusi3146 located in 43
+(11,11) : taurusi3147 located in 44
+(23,23) : taurusi3149 located in 45
+(35,35) : taurusi3148 located in 46
+(47,47) : taurusi3146 located in 47
+EOF
+
+// In order to test if it works programmatically
+val nGpus = 12
+val gpuHostAssignment = sc.parallelize( 0 to 4*nGpus-1, 4*nGpus-1 ).
+    map( x=>(x,x) ).
+    partitionBy( new GroupPartitioner(4*nGpus,4*nGpus) ).
+    map( x => {
+        Thread.sleep(10)
+        ( InetAddress.getLocalHost().getHostName(), List( x._1 % nGpus ) )
+    } ).
+    reduceByKey( _ ++ _ )
+try {
+    gpuHostAssignment.foreach( x => assert( x._2 == List.range( 0, nGpus )  ) )
+} catch { case _:AssertionError => println("GPU Partitioning not working as expected.") }
+gpuHostAssignment.collect().foreach( x => println( "Host "+x+" use GPU " ) )
+
+// get Number of workers
+val nExecutors = sc.getExecutorMemoryStatus.size - 1
+var nParts = 1
+var nExes = 0
+var nExesNew = 0
+do {
+    nExes = nExesNew
+    nExesNew = sc.parallelize( 0 to nParts, nParts ).map( x=>(x,x) ).
+        partitionBy( new ExactPartitioner( nParts, nParts ) ).
+        map( x => ( InetAddress.getLocalHost().getHostName(), 1 ) ).
+        reduceByKey(_+_).collect.size
+    nParts = nParts * 2
+} while( nExes != nExesNew )
+println( nExesNew )
+assert( nExecutors == nExesNew )
