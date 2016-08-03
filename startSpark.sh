c8167fb476d5debbc80f2cd7483ca1519c04e528
mxmlnkn <mxmlnkn@github.de>
2016-08-03 22:53:40 +0200

    comment startSpark script, dunno how to get the path of the file, if sourced (pwd does not work, because it could be source using folders in path

--- startSpark.sh
+++ startSpark.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
+# Define SPARK_ROOT variable to point to the root folder of Spark (that where
+# the folder 'bin' resides in), e.g. add that variable in your .bashrc.
+# Source this script, possibly also in your .bashrc, and then you can use simply
+#
+#   startSpark [Slurm Parameters]
+#
+# E.g.:
+#
+#   startSpark --time=11:00:00 --nodes=8 --partition=gpu2 --cpus-per-task=4 --gres='gpu:4'
+#
+# Don't choose anything else than --ntasks-per-node=1, because each SLURM
+# task will start one Spark Executor. If nothing specified 1 is the default.
+#
+# Each Spark Executor will run --cpus-per-task threads. This is the parallelism
+# availabe by using Spark Partitions.
+#
+# So if you want each partition to work on another GPU you will have to watch
+# out that --cpus-per-task=4 and --gres='gpu:4' have the same number!
+# But maybe you want only 1 thread per node where each thread distributes it's
+# work manually to GPU, then you can experiment with --cpus-per-task=1 and
+# --gres='gpu:4'
+
+export START_SPARK_SLURM_PATH=$HOME/scaromare/common/start_spark_slurm.sh
+
 function startSpark() {
     export SPARK_LOGS=$HOME/spark/logs
     mkdir -p "$SPARK_LOGS"
     if [ ! -d "$SPARK_LOGS" ]; then return 1; fi
-    jobid=$(sbatch "$@" --output="$SPARK_LOGS/%j.out" --error="$SPARK_LOGS/%j.err" $HOME/scaromare/start_spark_slurm.sh)
+    jobid=$(sbatch "$@" --output="$SPARK_LOGS/%j.out" --error="$SPARK_LOGS/%j.err" $START_SPARK_SLURM_PATH)
     jobid=${jobid##Submitted batch job }
     echo "Job ID : $jobid"
-    # looks like: 16/05/13 20:44:59 INFO MasterWebUI: Started MasterWebUI at http://172.24.36.19:8080
+    # looks like: 16/05/13 20:44:59 INFO MasterWebUI: Started MasterWebUI at http://123.123.123.123:8080
     echo -n "Waiting for Job to run and Spark to start.."
     MASTER_WEBUI=''
     while [ -z "$MASTER_WEBUI" ]; do
@@ -21,7 +45,7 @@ function startSpark() {
     export MASTER_WEBUI
     export MASTER_ADDRESS=$(cat ~/spark/logs/${jobid}_spark_master)
     function sparkSubmit() {
-        ~/spark-1.5.2-bin-hadoop2.6/bin/spark-submit --master $MASTER_ADDRESS $@
+        "$SPARK_ROOT"/bin/spark-submit --master $MASTER_ADDRESS $@
     }
     cat "$SPARK_LOGS"/$jobid.*
     echo "MASTER_WEBUI   : $MASTER_WEBUI"
